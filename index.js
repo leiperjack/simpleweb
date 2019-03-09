aad5434e21a60125285ab0de1b33fb41d31be947
leiperjack <leiperjack1997@gmail.com>
2019-03-09 08:56:01 +0000

    updating message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('How are you doing?');
+    res.send('Hello');
 });
 
 app.listen(7000, () => {
